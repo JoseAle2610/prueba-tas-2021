a56620e4b251e386c9679a2535f73a66c413f4bf
JoseAle2610 <jalesgervazzi@gmail.com>
2021-12-29 09:24:43 -0400

    Small improvement at Inicio

--- src/Application/Inicio/index.js
+++ src/Application/Inicio/index.js
@@ -1,6 +1,8 @@
 const Inicio = () => {
   return (
-    <div style={{height: '100%'}}>Inicio</div>
+    <div style={{height: '89%'}} className='d-flex flex-row align-items-center justify-content-center'>
+      <h1 className='text-center d-inline-block'>Bienvenido a WebApp, donde podras comprar todo tipo de productos</h1>
+    </div>
   )
 }
 
